228d1d825cc32af83f6a64cf010d08d7c7bbe5a3
Gabe-S5 <gabrielsalisi15@gmail.com>
2021-03-01 18:01:38 -0500

    difficulty balancing

--- public/enemyClass.js
+++ public/enemyClass.js
@@ -1,29 +1,35 @@
-function enemy(x, y, grids) {
-    this.pos   = createVector(x,y)
-    this.alive = true
-    this.value = 5
-    this.maxHP = 100 + level * 5
-    this.health = this.maxHP
-
-    // Velocity
-    this.speed = 2 + (level * 0.2)
-    this.vel   = createVector(0,this.speed)
-
-    // Enemy Color
-    if (level % 2 === 0) { this.color = [255,0,255] }
-    else { this.color = [0,255,255] }
+class enemy {
+    constructor(x, y, grids, level) {
+        this.pos   = createVector(x,y)
+        this.alive = true
+        this.value = 5
+        this.level = level
+        this.maxHP = Math.pow(2, level) + 100
+        this.health = this.maxHP
+        this.grids = grids
+    
+        // Velocity
+        this.speed = this.grids/30 + Math.sqrt(level)
+        this.vel   = createVector(0,this.speed)
+    
+        // Enemy Color
+        if (level % 2 === 0) { this.color = [255,0,255] }
+        else { this.color = [0,255,255] }
+    
+        this.turn = 0
+    }
 
     // Draw Enemy
-    this.draw = function() {
+    draw() {
         fill(this.color)
-        ellipse(this.pos.x, this.pos.y, grids, grids)
+        ellipse(this.pos.x, this.pos.y, this.grids, this.grids)
         if (this.health < this.maxHP && this.health >= 0) {
             fill(255,0,0)
-            rect(this.pos.x - grids/2, this.pos.y + grids - 10, grids * (this.health/this.maxHP), 10)
+            rect(this.pos.x - this.grids/2, this.pos.y + this.grids - 10, this.grids * (this.health/this.maxHP), 10)
         }
     }
 
-    this.update = function() {
+    update() {
         if (this.health <= 0) {
             money += this.value
             this.kill()
@@ -31,43 +37,42 @@ function enemy(x, y, grids) {
     }
 
     // Moving enemy function
-    this.turn = 0
-    this.move = function() {
+    move() {
         switch (this.turn) {
             case 0:
-                if (this.pos.y >= grids*10) { this.turn += 1 }
+                if (this.pos.y >= this.grids*10) { this.turn += 1 }
                 break
             case 1:
                 this.vel = createVector(this.speed,0)
-                if (this.pos.x >= grids*10) { this.turn += 1 }
+                if (this.pos.x >= this.grids*10) { this.turn += 1 }
                 break
             case 2: 
                 this.vel = createVector(0,-this.speed)
-                if (this.pos.y <= grids*6) {this.turn += 1 }
+                if (this.pos.y <= this.grids*6) {this.turn += 1 }
                 break
             case 3:
                 this.vel = createVector(-this.speed, 0)
-                if (this.pos.x <= grids*6) { this.turn += 1 }
+                if (this.pos.x <= this.grids*6) { this.turn += 1 }
                 break
             case 4:
                 this.vel = createVector(0, -this.speed)
-                if (this.pos.y <= grids*1) { this.turn += 1 }
+                if (this.pos.y <= this.grids*1) { this.turn += 1 }
                 break
             case 5:
                 this.vel = createVector(this.speed, 0)
-                if (this.pos.x >= grids*16) { this.turn += 1 }
+                if (this.pos.x >= this.grids*16) { this.turn += 1 }
                 break
             case 6:
                 this.vel = createVector(0, this.speed)
-                if (this.pos.y >= grids*10) { this.turn += 1}
+                if (this.pos.y >= this.grids*10) { this.turn += 1}
                 break
             case 7:
                 this.vel = createVector(this.speed, 0)
-                if (this.pos.x >= grids*24) { this.turn += 1}
+                if (this.pos.x >= this.grids*24) { this.turn += 1}
                 break
             case 8: 
                 this.vel = createVector(0, -this.speed)
-                if (this.pos.y <= 0 - grids) { this.turn += 1}
+                if (this.pos.y <= 0 - this.grids) { this.turn += 1}
                 break
             case 9:
                 this.exit()
@@ -77,12 +82,12 @@ function enemy(x, y, grids) {
     }
 
     // Gives money for killing
-    this.kill = function() {
+    kill() {
         this.alive  = false
     }
 
     // Reduces lives on exit
-    this.exit = function() {
+    exit() {
         lives      -= 1
         this.alive  = false
     }
